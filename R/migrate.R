20f4425600f79bd55895b9491921689172ce2b6f
Paul Burch <Paul.Burch@aad.gov.au>
2017-06-05 13:55:46 +1000

    package doc file

---
+++ R/migrate.R
@@ -0,0 +1,15 @@
+#' migrate: A package to estimate the migration rate between two areas from
+#' tagging and population data
+#'
+#' The migrate package implements a modification of Hilborn (1990) conditioned on
+#' catch instead of effort.
+#'
+#' @section Vignettes:
+#' To learn more about migrate, start with the vignettes:
+#' \code{browseVignettes(package = "migrate")}
+#'
+#' @docType package
+#' @importFrom stats dmultinom optim
+#' @name migrate
+NULL
+#> NULL
